4fd5c93329ebd972eccacb243d4297a0afdb080d
Ubuntu <ubuntu@ip-172-31-19-110.us-west-2.compute.internal>
2016-03-26 07:27:12 +0000

    edit file server/config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://52.37.123.21:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
